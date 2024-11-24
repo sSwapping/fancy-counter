798b44bc03bee413c11b291835af649e0a71432f
SimpSwapping <154761856+SimpSwapping@users.noreply.github.com>
2024-11-24 15:07:04 +0100

    📱Reworked design also added a tooltip for the shortcuts on PC

--- src/components/CurrentCount.jsx
+++ src/components/CurrentCount.jsx
@@ -1,12 +1,6 @@
 function CurrentCount({ currentCount }) {
   return (
-    <p
-      className='text-[rgb(22,34,32)]
-        text-[15vw] sm:text-[120px] md:text-[160px] lg:text-[200px]
-        font-semibold
-        mt-auto sm:mt-[10px]
-        mb-auto'
-    >
+    <p className='text-[rgb(22,34,32)] text-[15vw] sm:text-[120px] md:text-[160px] lg:text-[200px] font-semibold mt-auto sm:mt-[10px] mb-auto'>
       {currentCount}
     </p>
   );
